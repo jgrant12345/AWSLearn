20e0af4743412ac1fa8761fbc2a4accb3fb513e9
jgrant12345 <justingrant99@me.com>
2025-01-05 22:21:42 -0800

    change

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
-const { DynamoDBClient, GetItemCommand } = require("@aws-sdk/client-dynamodb"); // CommonJS import
 const express = require('express')
 
 const app = express()
 const port = 8080
 
-const client = new DynamoDBClient({region: "us-west-2"});
-
-async function getPokemon(){
-  const input = {
-    "Key": {
-      "Name": {
-        "S" : "Tododile"
-      }
-    },
-    "TableName": "Pokemon"
-  }
-  const command = new GetItemCommand(input);
-  const response = await client.send(command);
-  return response;
-
-}
-
 app.get('/', async (req, res) => {
-  const newPokemon = await getPokemon();
-  console.log(newPokemon["Item"]["Name"]["S"])
-  res.send("Fortune of the day app coming soon!!!!")
+  res.send("Fortune of the day!")
 })
 
 app.listen(port, () => {
